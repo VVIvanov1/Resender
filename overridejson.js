429978152cb84fad818b9e8cd8901f6b8a8598de
BILETALU <[ivanovv.tca@gmail.com]>
2021-09-16 23:17:51 +0600

    add json override test function

--- overridejson.js
+++ overridejson.js
@@ -3,12 +3,26 @@ const fs = require('fs').promises
 
 const file = path.join(__dirname, 'test.json')
 
-fs.readFile(file)
-.then(body => JSON.parse(body))
-.then(json => {
-    json.person.name = 'Vladislav'
-    return json
-})
-.then(json => JSON.stringify(json))
-.then(body => fs.writeFile(file, body))
-.catch(error => console.error())
+let obj = {
+    name: 'vlad',
+    surename: 'petrov'
+}
+
+function changeJson(path2file, params) {
+    fs.readFile(path2file)
+        .then(body => JSON.parse(body))
+        .then(json => {
+            let keys = Object.keys(params)
+            console.log(keys);
+            keys.map(k => {
+                json.person[k] = params[k]
+            })
+            console.log(json);
+            return json
+        })
+        .then(json => JSON.stringify(json))
+        .then(body => fs.writeFile(file, body))
+        .catch(error => console.error())
+}
+changeJson(file, obj)
+
