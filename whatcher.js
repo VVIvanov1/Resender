2fef755391466c92cfbafba44eccc3721b014845
= <=>
2021-09-16 14:13:36 +0600

    add whatcher func to ave multiple files to gDrive

--- whatcher.js
+++ whatcher.js
@@ -1,21 +1,37 @@
-// const fs = require('fs')
 const path = require('path')
-const hound = require('hound');
-const pathToFolder = 'C:\\Users\\Biletalu\\Desktop\\Новая папка\\Новая папка'
 
-// fs.watch(pathToFolder, (event, file)=>{
-//    console.log(event);
-//    console.log(file);
-// })
+const fs = require('fs');
+const pathToFolder = 'C:\\Users\\user\\Desktop\\Новая папка\\Новая папка'
+const funcs = require('./functions')
 
-const watcher = hound.watch(pathToFolder)
+const hound = require('hound')
 
-watcher.on('create', (file, stats) => {
-    console.log(path.basename(file)  + ' was created');
-    console.log(path.dirname(file));
-})
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')))
 
-watcher.on('delete', (file, stats) => {
-    console.log(path.basename(file)  + ' was deleted');
-    console.log(path.dirname(file));
-})
+
+
+hound.watch(pathToFolder)
+    .on('create', (file, stats) => {
+        console.log(path.basename(file) + ' was created');
+        console.log(path.dirname(file));
+        const gFolder = config.configuration.mirFolderGoogle;
+        fs.readdir(pathToFolder, (err, files) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const processed = files.map(file => path.join(pathToFolder, file))
+                processed.forEach(file => {
+                    let name = path.basename(file)
+                    funcs.saveToFolder(gFolder, name, file)
+                })
+
+            }
+        })
+    })
+
+    .on('delete', (file, stats) => {
+        console.log(path.basename(file) + ' was deleted');
+        console.log(path.dirname(file));
+    })
+
+
