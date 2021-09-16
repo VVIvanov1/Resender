d5793215470d67e2bace304e6ea4170e28129baf
= <=>
2021-09-16 11:43:21 +0600

    fyle system function to work with files

---
+++ filesystem.js
@@ -0,0 +1,23 @@
+
+const fs = require('fs');
+const path = require('path')
+const pathToFolder = 'C:\\Users\\user\\Desktop\\Новая папка\\Новая папка'
+const paTH = 'C:/Users/user/Desktop/vse/web site contents/certificates'
+
+
+
+function getFilestList() {
+    fs.readdir(pathToFolder, (err, files) => {
+
+
+        if (err) {
+            console.log(err);
+        } else {
+           let processed =  files.map(file => path.join(pathToFolder, file))
+            console.log(processed);
+        }
+
+    })
+}
+
+getFilestList()
